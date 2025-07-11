38972b2a2d2b6a1a1c0734076a4390a52501a95b
h3nrzi <rezaeig22@gmail.com>
2025-07-11 19:52:44 +0330

    refactor: enable Invoice and Plan repositories in index file for better repository management

--- src/db/repositories/index.ts
+++ src/db/repositories/index.ts
@@ -1,9 +1,13 @@
 import { AppDataSource } from "../data-source";
+import { Invoice } from "../entities/Invoice";
+import { Plan } from "../entities/Plan";
 import { User } from "../entities/User";
+import { InvoiceRepository } from "./InvoiceRepository";
+import { PlanRepository } from "./PlanRepository";
 import { UserRepository } from "./UserRepository";
 
 const userRepo = new UserRepository(AppDataSource.getRepository(User));
-// const planRepo = new PlanRepository(AppDataSource.getRepository(Plan));
-// const invoiceRepo = new InvoiceRepository(AppDataSource.getRepository(Invoice));
+const planRepo = new PlanRepository(AppDataSource.getRepository(Plan));
+const invoiceRepo = new InvoiceRepository(AppDataSource.getRepository(Invoice));
 
-export { userRepo };
+export { invoiceRepo, planRepo, userRepo };
